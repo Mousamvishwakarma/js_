dd6a9790f6326b11eb4202eb8ed37065b3b498f1
MousamVishwakarma <mousamvishwakarma929@gmail.com>
2025-08-22 08:18:21 +0000

    commits

---
+++ 01_basices/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 78789
+let accountEmail = "mousma@golle.com"
+var accountPassword = "6546321456"
+accoumtCity ="indore"
+
+console.table([accountEmail, accountId,accountPassword,accoumtCity]);
