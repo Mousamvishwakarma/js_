850a6a925a2fafd5c2f3f2005f005c967545a499
MousamVishwakarma <mousamvishwakarma929@gmail.com>
2025-08-23 06:03:09 +0000

    my commit

---
+++ datatypes/datatypes-sumrry.js
@@ -0,0 +1,19 @@
+const id = Symbol ('456789')
+const anotherId = Symbol(456789)
+console.log(id === anotherId);
+
+const bigNumber = 789456412265613n
+console.log(bigNumber);
+
+
+
+const heros = ["shart iman", "naagraj", "doga"]
+ 
+let myO={
+    name: "mousam",
+    age: 22,
+}
+const  myFunction= function(){
+    console.log("hello !");
+    
+}
