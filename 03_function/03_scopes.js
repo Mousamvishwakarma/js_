d9f4b6fde58e0045be30bc70dd66808438b48da6
mousamvishwakarma <mousamvishwakarma2005@gmail.com>
2025-08-31 00:21:47 +0530

    commit

---
+++ 03_function/03_scopes.js
@@ -0,0 +1,15 @@
+let a=200
+
+if (true) {
+    let a = 52
+    const b = 10
+    console.log("inner", a);
+    
+}
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+console.log(a);
