0de451c701e9472d00650d3f33ae1e639f745426
mousamvishwakarma <mousamvishwakarma2005@gmail.com>
2025-08-30 20:20:21 +0530

    Add course object with instructor and logging statements

---
+++ 02_basices/05_object.js
@@ -0,0 +1,10 @@
+const course = {
+    coursename: "js ",
+    price: "9999999",
+    courseInstructor: "mousam"
+}
+const {courseInstructor} = course 
+console.log(courseInstructor);
+console.log(course);
+
+
