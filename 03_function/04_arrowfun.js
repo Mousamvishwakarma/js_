86f390da098e1b091bf071b354cc81880dd32c46
mousamvishwakarma <mousamvishwakarma2005@gmail.com>
2025-08-31 00:31:33 +0530

    new

---
+++ 03_function/04_arrowfun.js
@@ -0,0 +1,10 @@
+const user = {
+    username: "mmousam",
+    price: 999,
+
+    welcomeMassage: function (){
+        console.log(`${this.username} , welcome `);
+        
+    }
+}
+user.welcomeMassage
