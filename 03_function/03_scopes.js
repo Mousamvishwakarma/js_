29440e0b1a6472519150ccd712607d7416ebb7e1
mousamvishwakarma <mousamvishwakarma2005@gmail.com>
2025-08-31 20:35:16 +0530

    Refactor variable declarations and enhance function structure in 03_scopes.js

--- 03_function/03_scopes.js
+++ 03_function/03_scopes.js
@@ -1,15 +1,54 @@
-let a=200
+let a = 200
 
 if (true) {
     let a = 52
     const b = 10
     console.log("inner", a);
-    
+
 }
 
-// for (let i = 0; i < array.length; i++) {
-//     const element = array[i];
-    
-// }
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+
+}
 
 console.log(a);
+
+function one() {
+    const username = " mousam"
+
+    function two() {
+        const website = " youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+one()
+
+if (true) {
+    const username = "mousam "
+    if (username === "mousam ") {
+        const website = "youtube"
+        console.log(username + website);
+        
+    }
+    console.log(website);
+    
+}
+console.log(username);
+
+function addone (num){
+    return num + 1
+}
+console.log(addone(5));
+
+
+
+
+const addtwo = function (num){
+
+    return num + 5
+}
+console.log(addtwo(5));
