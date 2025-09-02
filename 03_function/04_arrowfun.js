6e4e69d2a43c85338b2c58c9dbe666e8d17b95cc
mousamvishwakarma <mousamvishwakarma2005@gmail.com>
2025-09-02 23:50:21 +0530

    #

--- 03_function/04_arrowfun.js
+++ 03_function/04_arrowfun.js
@@ -3,8 +3,38 @@ const user = {
     price: 999,
 
     welcomeMassage: function (){
-        console.log(`${this.username} , welcome `);
+        // console.log(`${this.username} , welcome `);
+        // console.log(this);
         
     }
 }
-user.welcomeMassage
+// user.welcomeMassage
+// user.username = "sam"
+// user.welcomeMassage()
+
+// console.log(this);
+
+
+// function chai(){
+//     let username = "mousam"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+const chai = () =>{
+      let username = "mousam"
+   console.log(this.username);
+}
+chai()
+
+// const addTwo =(num1 , num2) =>{
+//     return num1 + num2;
+// }
+// console.log(addTwo(5,8));
+
+// const addthree = (num1 , num2) => num1 + num2 
+// console.log(addthree(5,9));
+
+const addnum = (num1 , num2) => ({username:" mousam"})
+console.log(addnum(5,9));
---
+++ 03_function/05_iffe.js
@@ -0,0 +1,12 @@
+(function  chai (){
+    console.log("DB Connected");
+    
+})
+();
+
+((name) =>{
+     name = "mousam"
+    console.log(`DS CONNECTED  ${name}`);
+  
+})
+()
