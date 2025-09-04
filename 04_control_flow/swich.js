2ecfc0912cf49b9cd19c0cbd9e02a8e807794323
mousamvishwakarma <mousamvishwakarma2005@gmail.com>
2025-09-04 22:50:58 +0530

    #

---
+++ 04_control_flow/swich.js
@@ -0,0 +1,58 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = "2"
+// switch (month) {
+//     case 1:
+//         console.log("january");
+        
+//         break;
+//     case 2:
+//         console.log("feb");
+        
+//         break;
+//     case 3:
+//         console.log("march");
+        
+//         break;
+//     case 4:
+//         console.log("april");
+        
+//         break;
+
+//     default:
+//         console.log("default caase");
+        
+//         break;
+// }
+
+const month = "feb"
+switch (month) {
+    case "jan":
+        console.log("january");
+        
+        break;
+    case "feb":
+        console.log("feb");
+        
+        break;
+    case "march":
+        console.log("march");
+        
+        break;
+    case "april":
+        console.log("april");
+        
+        break;
+
+    default:
+        console.log("default caase");
+        
+        break;
+}
