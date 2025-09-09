43b0772f14076376d8b1f8e8df9c221892705f3e
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-09 16:18:20 +0530

    Add chaicode.js for random background color changer functionality with start and stop controls

---
+++ 08_events/chaicode.js
@@ -0,0 +1,29 @@
+const randomColor = function () {
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color
+};
+//  console.log(randomColor());
+let intervalId;
+const startchangingColor = function () {
+    intervalId = setInterval(changeBgColor, 1000);
+
+    function changeBgColor() {
+        document.body.style.backgroundColor = randomColor();
+    }
+};
+const stopchangingColor = function () {
+    clearInterval(intervalId)
+    intervalId = null;
+};
+
+
+document.querySelector('#start').addEventListener
+    ('click', startchangingColor);
+
+document.querySelector('#stop').addEventListener
+    ('click', stopchangingColor);
+
