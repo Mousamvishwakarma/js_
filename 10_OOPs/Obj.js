34441bfdc46becd50dc455240c2ddc569405548c
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-13 23:08:06 +0530

    object problems

---
+++ 10_OOPs/Obj.js
@@ -0,0 +1,15 @@
+
+
+const  user = {
+    usename: " mousam",
+    loginCount: 5,
+    signedIn: true,
+
+    getUserD: function(){
+        console.log(" Got user details");
+        
+    }
+}
+console.log(user.usename);
+console.log(user.getUserD);
+
