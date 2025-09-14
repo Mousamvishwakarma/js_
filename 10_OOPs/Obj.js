89f751a3a6109a70f6d76193b1abfec93570ad11
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-14 14:32:06 +0530

    #

--- 10_OOPs/Obj.js
+++ 10_OOPs/Obj.js
@@ -7,11 +7,23 @@ const  user = {
 
     getUserD: function(){
         // console.log(" Got user details");
-        console.log(`username ${this.username}`);
+        // console.log(`username ${this.username}`);
+        console.log(this);
         
     }
 }
 console.log(user.usename);
-console.log(user.getUserD);
+// console.log(user.getUserD);
+console.log(this);
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+    
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
 
 
