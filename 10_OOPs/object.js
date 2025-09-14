8308163a3935f9e73f13a32a85be77c23ea292cd
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-14 21:12:30 +0530

    #

--- 10_OOPs/object.js
+++ 10_OOPs/object.js
@@ -1,8 +1,27 @@
-function getmultipleby5 (num){
-    return num*5
+function getmultipleby5(num) {
+    return num * 5
 
 }
 getmultipleby5.power = 2
 console.log(getmultipleby5(5));
 console.log(getmultipleby5.power);
 console.log(getmultipleby5.prototype);
+
+function createFunc(username, score) {
+    this.username = username
+    this.score = score
+}
+createFunc.prototype.increment = function () {
+    this.score++
+}
+createFunc.prototype.printme =function(){
+    console.log(`price is ${this.score}`);
+    
+}
+const chai =new  createFunc("chai ",25);
+const tea = new createFunc("tea ",250);
+
+chai.printme()
+
+tea.printme()
+
