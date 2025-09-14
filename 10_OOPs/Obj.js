66653c265a30b30d866262f6d9178b56e0980043
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-14 20:37:00 +0530

    new file add this return

--- 10_OOPs/Obj.js
+++ 10_OOPs/Obj.js
@@ -1,29 +1,40 @@
-
-
-const  user = {
-    usename: " mousam",
-    loginCount: 5,
+const user = {
+    username: "Mousam",
+    loginCount: 8,
     signedIn: true,
 
-    getUserD: function(){
-        // console.log(" Got user details");
-        // console.log(`username ${this.username}`);
+    getUserDetails: function(){
+        console.log("Got user details from database");
+        console.log(`Username: ${this.username}`);
         console.log(this);
-        
     }
+
 }
-console.log(user.usename);
-// console.log(user.getUserD);
+
+
+
+console.log(user.username)
+console.log(user.getUserDetails());
 console.log(this);
+
 
 function User(username, loginCount, isLoggedIn){
     this.username = username;
     this.loginCount = loginCount;
     this.isLoggedIn = isLoggedIn
-    
+
     this.greeting = function(){
         console.log(`Welcome ${this.username}`);
 
     }
 
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
 
+    }
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+console.log(userTwo);
+}
