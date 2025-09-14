4263258fb2aa4e0d3a79ec448a6a36be3f2dcd67
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-14 20:41:56 +0530

    #

---
+++ 10_OOPs/object.js
@@ -0,0 +1,8 @@
+function getmultipleby5 (num){
+    return num*5
+
+}
+getmultipleby5.power = 2
+console.log(getmultipleby5(5));
+console.log(getmultipleby5.power);
+console.log(getmultipleby5.prototype);
