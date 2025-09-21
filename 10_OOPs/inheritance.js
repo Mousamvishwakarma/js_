f62dd506516812a11f467e316e977e1500966772
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-21 23:45:26 +0530

    inheritance in js

--- 10_OOPs/inheritance.js
+++ 10_OOPs/inheritance.js
@@ -1,10 +1,31 @@
-class User{
+
+class User {
     constructor(username){
         this.username = username
-
     }
-    callMe(){
-        console.log(`username is ${this.username}`);
-        
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
     }
 }
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.logMe()
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai instanceof User);
