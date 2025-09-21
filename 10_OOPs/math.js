8e9028e6df050e864eb513fb4f7d43558d955399
mousamvishwakarma <mousamvishwakarma929@gmail.com>
2025-09-21 23:39:28 +0530

    #

---
+++ 10_OOPs/math.js
@@ -0,0 +1,17 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
